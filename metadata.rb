f5ab37bf53122aa5d0cfb703b1899ee819747b2f
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2013-02-15 02:39:52 +1100

    Depend on hosts (for hosts_entry LWRP)

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,10 @@
-description 'Sets and persists a hostname, manages /etc/hosts'
+maintainer       'Vasily Mikhaylichenko'
+maintainer_email 'vasily.mikhaylichenko+github@gmail.com'
+license          'BSD'
+description      'A Chef cookbook to set and persist node hostname'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.3.0'
+
 %w{ ubuntu debian redhat centos gentoo}.each { |os| supports os }
+
+depends 'hosts'
