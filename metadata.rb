b98fdfb10ff0128fd688ed8e9a7f6f1e91254d66
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2013-02-20 09:52:32 +1100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'vasily.mikhaylichenko+github@gmail.com'
 license          'BSD'
 description      'A Chef cookbook to set and persist node hostname'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 %w{ ubuntu debian redhat centos gentoo}.each { |os| supports os }
 
