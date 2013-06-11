ad58b3ce06b42a73e1833ed670e3a99041e1b78a
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2013-06-11 13:48:14 +1000

    Release 0.3.2

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.3.2 / 2013-06-11
+
+Initial public release with test-kitchen integration.
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'hostname'
+name             'lxmx-hostname'
 maintainer       'Vasily Mikhaylichenko'
 maintainer_email 'vasily.mikhaylichenko+github@gmail.com'
 license          'BSD'
 description      'A Chef cookbook to set and persist node hostname'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.3.2'
 
 %w{ ubuntu debian redhat centos gentoo}.each { |os| supports os }
 
