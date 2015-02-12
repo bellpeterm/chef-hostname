ff1e7b9a1224293746494a1c0ad39d443f251de1
Peter Bell <peter_bell@peterb-mbp.local>
2015-02-12 15:37:38 -0800

    remove overzealous checks

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,66 +3,61 @@ ohai 'reload' do
 end
 
 if node['net']
-  require 'resolv'
-
   fqdn         = node['net']['FQDN']
   the_hostname = node['net']['hostname']
   ip           = node['net']['IP']
 
-  if node['hostname'] != the_hostname || node['fqdn'] != fqdn
-    # Update the hostname
-    case node['platform']
-    when 'ubuntu', 'debian'
-      file '/etc/hostname' do
-        content "#{fqdn}\n"
-        mode '0644'
-      end
-    when 'redhat', 'centos'
-      ruby_block 'edit /etc/sysconfig/network' do
-        block do
-          rc = Chef::Util::FileEdit.new('/etc/sysconfig/network')
-          rc.search_file_replace_line(/^HOSTNAME/, "HOSTNAME=#{fqdn}")
-          rc.write_file
-        end
-      end
-    when 'gentoo'
-      file '/etc/conf.d/hostname' do
-        content "HOSTNAME=\"#{the_hostname}\"\n"
-      end
+  # Update the hostname
+  case node['platform']
+  when 'ubuntu', 'debian'
+    file '/etc/hostname' do
+      content "#{fqdn}\n"
+      mode '0644'
+      not_if { open('/etc/sysconfig/network') { |f| f.grep(/string/) } }
     end
-
-    hostsfile_entry '127.0.0.1' do
-      hostname the_hostname
-      aliases ['localhost', 'localhost.localdomain']
+  when 'redhat', 'centos'
+    ruby_block 'edit /etc/sysconfig/network' do
+      block do
+        rc = Chef::Util::FileEdit.new('/etc/sysconfig/network')
+        rc.search_file_replace_line(/^HOSTNAME/, "HOSTNAME=#{fqdn}")
+        rc.write_file
+      end
+      not_if { open('/etc/sysconfig/network') { |f| f.grep(/string/) } }
     end
-
-    execute "hostname #{fqdn}" do
-      notifies :reload, 'ohai[reload]', :immediately
+  when 'gentoo'
+    file '/etc/conf.d/hostname' do
+      content "HOSTNAME=\"#{the_hostname}\"\n"
+      not_if { open('/etc/conf.d/hostname') { |f| f.grep(/string/) } }
     end
   end
 
-  resolver = Resolv::DNS.new
+  hostsfile_entry '127.0.0.1' do
+    hostname the_hostname
+    aliases ['localhost', 'localhost.localdomain']
+  end
+
+  execute "hostname #{fqdn}" do
+    notifies :reload, 'ohai[reload]', :immediately
+    not_if { node['fqdn'] == fqdn }
+  end
 
   case node['platform']
   when 'ubuntu', 'debian'
     hostsfile_entry '127.0.1.1' do
       hostname fqdn
       aliases [ the_hostname ]
-      not_if { resolver.getaddress(fqdn).to_s.match(/127\.0/) }
       notifies :reload, 'ohai[reload]', :immediately
     end
   when 'redhat', 'centos'
     hostsfile_entry ip do
       hostname fqdn
       aliases [ the_hostname ]
-      not_if { resolver.getaddress(fqdn).to_s.match(ip) }
       notifies :reload, 'ohai[reload]', :immediately
     end
   when 'gentoo'
     hostsfile_entry '127.0.0.1' do
       hostname fqdn
       aliases [ the_hostname, 'localhost.localdomain', 'localhost' ]
-      not_if { resolver.getaddress(fqdn).to_s.match(/127\.0/) }
       notifies :reload, 'ohai[reload]', :immediately
     end
   end
