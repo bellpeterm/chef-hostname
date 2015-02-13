40260ff04407a5935842498765125cd3fab3c364
Peter Bell <peter_bell@peterb-mbp.local>
2015-02-13 10:17:51 -0800

    further idempotence fixes; remove unecessary local variables

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,63 +2,60 @@ ohai 'reload' do
   action :nothing
 end
 
-if node['net']
-  fqdn         = node['net']['fqdn']
-  the_hostname = node['net']['hostname']
-  ip           = node['net']['ip']
+puts node['net']
 
-  # Update the hostname
-  case node['platform_family']
-  when 'debian'
-    file '/etc/hostname' do
-      content "#{fqdn}\n"
-      mode '0644'
-      not_if { open('/etc/sysconfig/network') { |f| f.grep(/string/) } }
-    end
-  when 'rhel'
-    ruby_block 'edit /etc/sysconfig/network' do
-      block do
-        rc = Chef::Util::FileEdit.new('/etc/sysconfig/network')
-        rc.search_file_replace_line(/^HOSTNAME/, "HOSTNAME=#{fqdn}")
-        rc.write_file
-      end
-      not_if { open('/etc/sysconfig/network') { |f| f.grep(/string/) } }
-    end
-  when 'gentoo'
-    file '/etc/conf.d/hostname' do
-      content "HOSTNAME=\"#{the_hostname}\"\n"
-      not_if { open('/etc/conf.d/hostname') { |f| f.grep(/string/) } }
-    end
-  end
+if node['net']
 
   hostsfile_entry '127.0.0.1' do
     hostname 'localhost'
     aliases ['localhost.localdomain']
   end
 
-  execute "hostname #{fqdn}" do
-    notifies :reload, 'ohai[reload]', :immediately
-    not_if { node['fqdn'] == fqdn }
-  end
+  # Update the hostname
+  case node['platform_family']
+  when 'debian'
+    file '/etc/hostname' do
+      content "#{node['net']['FQDN']}\n"
+      mode '0644'
+      not_if { open('/etc/sysconfig/network') { |f| f.grep(/string/) } }
+    end
 
-  case node['platform']
-  when 'ubuntu', 'debian'
     hostsfile_entry '127.0.1.1' do
-      hostname fqdn
-      aliases [ the_hostname ]
+      hostname node['net']['FQDN']
+      aliases [ node['net']['hostname'] ]
       notifies :reload, 'ohai[reload]', :immediately
     end
-  when 'redhat', 'centos'
-    hostsfile_entry ip do
-      hostname fqdn
-      aliases [ the_hostname ]
+  when 'rhel'
+    ruby_block 'edit /etc/sysconfig/network' do
+      block do
+        rc = Chef::Util::FileEdit.new('/etc/sysconfig/network')
+        rc.search_file_replace_line(/^HOSTNAME/, "HOSTNAME=#{node['net']['FQDN']}")
+        rc.write_file
+      end
+      not_if { open('/etc/sysconfig/network') { |f| f.grep(/string/) } }
+    end
+
+    hostsfile_entry node['net']['IP'] do
+      hostname node['net']['FQDN']
+      aliases [ node['net']['hostname'] ]
       notifies :reload, 'ohai[reload]', :immediately
     end
   when 'gentoo'
+    file '/etc/conf.d/hostname' do
+      content "HOSTNAME=\"#{node['net']['hostname']}\"\n"
+      not_if { open('/etc/conf.d/hostname') { |f| f.grep(/string/) } }
+    end
+
     hostsfile_entry '127.0.0.1' do
-      hostname fqdn
-      aliases [ the_hostname, 'localhost.localdomain', 'localhost' ]
+      hostname node['net']['FQDN']
+      aliases [ node['net']['hostname'], 'localhost.localdomain', 'localhost' ]
       notifies :reload, 'ohai[reload]', :immediately
     end
   end
+
+  execute "hostname #{node['net']['FQDN']}" do
+    notifies :reload, 'ohai[reload]', :immediately
+    not_if { node['fqdn'] == node['net']['FQDN'] }
+  end
+
 end
